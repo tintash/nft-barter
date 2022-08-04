b76ff5897a23366ee0d025550b527b7a667000b9
Asim Mehmood <asim@tintash.com>
2022-08-05 01:27:47 +0500

    added erc nft-barter core contracts

---
+++ nft-barter-core/contracts/nft-barter.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts (last updated v4.7.0) (token/ERC721/ERC721.sol)
+
+pragma solidity ^0.8.0;
+
+import "./interfaces/IERC721.sol";
+
+//this contract is an implementation of IERC721 and is inspired by OpenZeppelin ERC721
+contract NftBarter is IERC721{
+
+}
